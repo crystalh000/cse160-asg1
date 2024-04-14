f638046758281c047eafdca708432211da4d3714
Crystal Huang <chuan147@ucsc.edu>
2024-04-13 17:03:57 -0700

    up to video 1.4b

--- asg1/ColoredPoints.js
+++ asg1/ColoredPoints.js
@@ -55,10 +55,30 @@ function connectVariablesToGLSL() {
   }
 }
 
+let g_selectedColor = [1.0,1.0,1.0,1.0]; // Default white
+
+// Set up actions for the HTML UI elements
+function addActionsForHTMLUI() {
+
+  // Button Events (Shape Type)
+  document.getElementById('green').onclick = function() { g_selectedColor = [0.0,1.0,0.0,1.0]; };
+  document.getElementById('red').onclick = function() {g_selectedColor = [1.0,0.0,0.0,1.0]; };
+
+  // Slider Events
+  document.getElementById('redSlide').addEventListener('mouseup',function() { g_selectedColor[0] = this.value/100;   });
+  document.getElementById('greenSlide').addEventListener('mouseup',function() { g_selectedColor[1] = this.value/100; });
+  document.getElementById('blueSlide').addEventListener('mouseup',function() { g_selectedColor[2] = this.value/100;  });
+
+}
 function main() {
+  // Set up canvas and get gl variables
   setUpWebGL();
 
+  // Set up GLSL shader programs and connect JS variables to GLSL
   connectVariablesToGLSL();
+
+  // Set up actions for the HTML UI Elements
+  addActionsForHTMLUI();
 
   // Register function (event handler) to be called on a mouse press
   canvas.onmousedown = click;
@@ -74,19 +94,21 @@ var g_points = [];  // The array for the position of a mouse press
 var g_colors = [];  // The array to store the color of a point
 
 function click(ev) {
-  [x,y] = convertCoordinatesEventToGL(ev);
+  let [x,y] = convertCoordinatesEventToGL(ev);
 
   // Store the coordinates to g_points array
   g_points.push([x, y]);
 
-  // Store the coordinates to g_points array
-  if (x >= 0.0 && y >= 0.0) {      // First quadrant
-    g_colors.push([1.0, 0.0, 0.0, 1.0]);  // Red
-  } else if (x < 0.0 && y < 0.0) { // Third quadrant
-    g_colors.push([0.0, 1.0, 0.0, 1.0]);  // Green
-  } else {                         // Others
-    g_colors.push([1.0, 1.0, 1.0, 1.0]);  // White
-  }
+  g_colors.push(g_selectedColor.slice());
+
+  // // Store the coordinates to g_points array
+  // if (x >= 0.0 && y >= 0.0) {      // First quadrant
+  //   g_colors.push([1.0, 0.0, 0.0, 1.0]);  // Red
+  // } else if (x < 0.0 && y < 0.0) { // Third quadrant
+  //   g_colors.push([0.0, 1.0, 0.0, 1.0]);  // Green
+  // } else {                         // Others
+  //   g_colors.push([1.0, 1.0, 1.0, 1.0]);  // White
+  // }
 
   // Draw every shape that is supposed to be in the canvas
   renderAllShapes();
